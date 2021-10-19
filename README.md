33c6d8ecdefd6b76b0db2e5caaabdeb0d6d2307b
abriz97 <andreabrizzi@live.it>
2021-10-19 09:33:58 +0100

    redesign directory

--- README.md
+++ README.md
@@ -1,15 +1,35 @@
 ## R0-methods-comparison
 
-Code and data to reproduce the analysis in "A comparative analysis of statistical methods to estimate the reproduction number in emerging epidemics with implications for the current COVID-19 pandemic", https://doi.org/10.1093/cid/ciaa1599.
+Code and data to reproduce the analysis in:
 
-***Code***
-- 'report_simulations.Rmd' contains the code to run the simulation experiments.
-- 'real_data.R' containst the code to run the analysis on COVID data.
-- 'methods.R' contains functions for a variety of methods that will return R0 estimates and associated 95% confidence intervals.
-- 'produce_simulations.R' makes use of two other scripts in order to generate simulated epidemic curves, save them in the data folder and visualise them.
-   - 'simulation.R' contains the code to produce stochastic SEIR simulation.
-   - 'BP_simulation.R' produces simulations according to a Branching Process.
- 
+### Code
 
-***Datasets***
-- The COVID data used is available at: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series
+The github directory is structured as follows:
+
+- The `scripts` directory contains all the file necessary to reproduce the analyses described in the paper, as well as the main and supplementary figures.
+- The `data` directory contains the SEIR simulations used in our analysis as well as the COVID-19 data obtained from the [Hopkins University TODO]( https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series)
+- The `results` data contains html reports with code and plots.
+
+#### `scripts`	
+
+- `report_simulations.Rmd` contains the code to run the simulation experiments.
+- `real_data.R` containst the code to run the analysis on COVID data.
+- `methods.R` contains helper functions that fit different methods for the estimation of $R_0$.
+- `produce_simulations.R` makes use the `simulation.R` script to generate simulated epidemic curves, save them in the data folder and visualise them
+
+#### `data`
+
+#### `results`
+
+
+
+### License
+
+- The code in this repository is licensed under the Creative Commons Attribution 4.0  International ([CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)) by TODO. 
+
+- The data in the `data/time_series_covid19_confirmed_global.csv` file obtained from the  [Johns Hopkins University](https://github.com/CSSEGISandData/COVID-19.) are redistributed under the Creative Commons Attribution 4.0  International ([CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)).  Copyright Johns Hopkins University 2020.
+
+### Acknowledgement
+
+### Funding
+
--- scripts/simulation.R
+++ scripts/simulation.R
@@ -52,7 +52,8 @@ simulate_seir <- function(
 
 
 
-
+# For more info on simulating a discrete-time approximation of a S(E)IR model, follow the link:
+# https://cran.r-project.org/web/packages/odin/vignettes/discrete.html
 
 
 #' Simulate a discrete-time approximation of a continuous-time, discrete-state stochastic S(E)IR model.
@@ -303,30 +304,89 @@ simulate_seir_ode <- function(
 
 
 
+##########################################################
+#=== Simulate according to a Poisson Branching Process
+##########################################################
 
-# ## Test
-# simulate_seir_stochastic(arnaught = 2.0, t_E = 4, t_I = 4, Npv = 1e6, S_init = Npv-10, E_init = 0, I_init = 10, n_t = 1000, n_steps_per_t = 10) -> stochastic
-# simulate_seir_ode(arnaught = 2.0, t_E = 4, t_I = 4, Npv = 1e6, S_init = Npv-10, E_init = 0, I_init = 10, n_t = 1000, n_steps_per_t = 10) -> ode
-# 
-# bind_rows(stochastic, ode, .id = c('method')) %>%
-#   mutate(method = factor(method, labels = c('stochastic', 'ode'))) %>%
-#   pivot_longer(S:dIR) %>%
-#   ggplot()+
-#   geom_line(aes(x = time, y = value, color = method)) +
-#   facet_wrap(.~name, scales = 'free_y') +
-# #   xlim(c(0, 200))
-# 
-# 
-# lapply(1:1000, function(xx) simulate_seir_example(arnaught = 2.0, t_E = 4, t_I = 4, Npv = 1e6, E_init = 0, I_init = 10, n_t = 200, n_steps_per_t = 10, method = 'stochastic')) %>%
-#   bind_rows() %>%
-#   group_by(time) %>%
-#   summarise_all(mean) -> stochastic
-#   
-# simulate_seir_example(arnaught = 2.0, t_E = 4, t_I = 4, Npv = 1e6, E_init = 0, I_init = 10, n_t = 200, n_steps_per_t = 10, method = 'ode') -> ode
-# bind_rows(stochastic, ode, .id = c('method')) %>%
-#   mutate(method = factor(method, labels = c('stochastic', 'ode'))) %>%
-#   pivot_longer(S:dIR) %>%
-#   ggplot()+
-#   geom_line(aes(x = time, y = value, color = method)) +
-#   facet_wrap(.~name, scales = 'free_y') +
-#   xlim(c(0, 200))
+# Each infected individual produces a random number of secondary infections simultaneously.
+# The generation time is distributed according to a discrete distribution
+# The distribution of offsprings is given by a Poisson(R_n)
+
+# == Input
+
+# R0
+# Generation distribution
+
+
+# == Settings
+
+# 3 columns matrix: time - parents - incidence.
+# - time denotes day
+# - parents denotes the number of primary infections which will cause secondary infections on a given day
+# - incidence denotes the number of new infections produced by parents on a given day
+
+# == How?
+# Parents ---(Poisson)--> Incidence ---(Generation time)--> Parents ...
+
+# For day in 1: end:
+
+# count the parents, and produce the new infections for the same day via Poisson realizations
+# incidence[t] <- sum(rpoisson(n_parents[t], R0))
+
+# take the new cases, and decide when they will cause new infections:
+# n_parents <- n_parents + rgeneration(incidence[t], t)
+
+rgeneration <- function(n, t, mean = 6.5, sd = .62, par){
+  
+  # reparametrise in terms of shape and scale
+  sh = mean^2/sd^2
+  sc = sd^2/mean
+  tmp <- table(t + round(rgamma(n, shape = sh, scale = sc)))
+  
+  for(i in 1:length(tmp)){
+    index <- as.numeric(names(tmp[i]))
+    count <- as.numeric(tmp)[i]
+    par[index] <- par[index] + count
+  }
+  
+  # return summary of determined days of infections
+  return(par)
+}
+
+
+simulation_BP_individual <- function(n_days, R0, seeds, Reporting_fraction = 1){
+  
+  # prepare vectors to store results
+  n_parents <- rep(0, n_days)
+  incidence <- rep(NA, n_days)
+  
+  #decide day of infections for the seeds
+  n_parents <- rgeneration(n = seeds, t = 0, par = n_parents)
+  
+  for (day in 1:n_days){
+    incidence[day] <- sum(rpois(n_parents[day], R0))
+    n_parents <- rgeneration(n = incidence[day], t = day, par = n_parents)
+  }
+  
+  n_parents <- n_parents[1:n_days]
+  obs_inc <- rbinom(n = n_days, size = incidence, prob = Reporting_fraction)
+  return(data.frame(day = 1:n_days, n_parents = n_parents, obs_incidence = obs_inc))
+}
+
+
+simulation_BP <- function(N, n_days = 50 , R0 = 3, seeds = 5, Reporting_fraction = 1){
+  
+  # Prepare list to store all the simulations
+  output <- data.frame()
+  
+  for (i in 1:N){
+    output <- rbind(output, simulation_BP_individual(n_days = n_days, R0 = R0, seeds = seeds, Reporting_fraction = Reporting_fraction)$obs_incidence)
+  }
+  
+  output <- t(output)
+  colnames(output) <- paste0("sim", 1:N)
+  rownames(output) <- c()
+  
+  return(output)
+  
+}
